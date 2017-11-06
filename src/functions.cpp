735c2d22945e22e698a2fe7dcb2165a3f0eb5baf
Carlos Domínguez García <alu0100966589@ull.edu.es>
2017-11-06 19:43:08 +0000

    Added parseInt and eraseWhiteSpaces functions

---
+++ src/functions.cpp
@@ -0,0 +1,41 @@
+#include "functions.hpp"
+
+int parseInt( std::string& str, int start, int end )
+{
+    int num = 0;
+    bool thereIsNoNumber = true;
+
+    while( start <= end )
+    {
+        if( str[start] <= '9'  &&  str[start] >= '0' )
+        {
+            thereIsNoNumber = false;
+            while( str[start] <= '9'  &&  str[start] >= '0' && start <= end )
+            {
+                // 48 is the ascii value for '0'
+                num += str[start] - 48;
+                num *= 10;
+                ++start;
+            }
+            break;
+        }
+        ++start;
+    }
+
+    if( thereIsNoNumber )
+        throw std::invalid_argument("There is no number in the string range");
+
+    return( num / 10 );
+}
+
+
+void eraseWhiteSpaces( std::string& str )
+{
+    auto endIterator = remove_if(
+        str.begin(),
+        str.end(),
+        []( char c ){ return c == ' '; }
+    );
+
+    str = std::string( str.begin(), endIterator );
+}
---
+++ src/functions.hpp
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+// Returns the number found in the range [start, end]
+// Throws an invalid_argument exception if no number is found
+int parseInt( std::string& str, int start, int end );
+
+// Removes every white space from a string
+void eraseWhiteSpaces( std::string& str );
+
+#endif
