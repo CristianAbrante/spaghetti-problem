e81eebdacc035fe68b246d90074df7d6283c83a1
Carlos Domínguez García <alu0100966589@ull.edu.es>
2017-11-06 23:45:24 +0000

    Added node struct and functions to determine node/statement type

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,6 +1,6 @@
 #include "functions.hpp"
 
-int parseInt( const std::string& str, int start, int end )
+int parseInt( const std::string &str, int start, int end )
 {
     int num = 0;
     bool thereIsNoNumber = true;
@@ -38,4 +38,71 @@ void eraseWhiteSpaces( std::string& str )
     );
 
     str = std::string( str.begin(), endIterator );
+}
+
+
+// Avoided to use regular expressions in these next functions
+// because they can cause trouble if the compiler is old.
+bool interpretIfStatement( Node &n )
+{
+    // "The language contains many other statements,
+    // but only the conditional goto begins with ‘if(’ and
+    // ends with ‘)goto label’"
+    int ifPos = n.statement.find("if(");
+    if( ifPos != 0  ||  ifPos == std::string::npos )
+        return false;
+
+    int gotoPos = n.statement.find(")goto", 3);
+    if( gotoPos == std::string::npos )
+        return false;
+
+    // Reached this point this has to be a conditional goto
+    // so the label must exist
+    n.labelToNextNode = parseInt( n.statement, gotoPos+5, n.statement.size()-1 );
+    n.statement.erase( gotoPos+5 );
+    n.type = StatementType::conditional;
+
+    return true;
+}
+
+
+bool interpretGotoStatement( Node &n )
+{
+    int gotoPos = n.statement.find("goto");
+    if( gotoPos != 0  ||  gotoPos == std::string::npos )
+        return false;
+
+    n.labelToNextNode = parseInt( n.statement, gotoPos+4, n.statement.size()-1 );
+    n.statement.erase( gotoPos+4 );
+    n.type = StatementType::unconditional;
+
+    return true;
+}
+
+
+bool interpretStopStatement( Node &n )
+{
+    if( n.statement != "stop" )
+        return false;
+
+    n.labelToNextNode = -1;
+    n.type = StatementType::stop;
+
+    return true;
+}
+
+
+void interpretOtherStatement( Node &n )
+{
+    n.labelToNextNode = -1;
+    n.type = StatementType::other;
+}
+
+
+void interpretStatement( Node &n )
+{
+    if( !interpretStopStatement(n) )
+        if( !interpretGotoStatement(n) )
+            if( !interpretIfStatement(n) )
+                interpretOtherStatement(n);
 }
--- src/functions.hpp
+++ src/functions.hpp
@@ -5,11 +5,50 @@
 #include <string>
 #include <algorithm>
 
-// Returns the number found in the range [start, end]
+// Returns the first number found in the range [start, end]
 // Throws an invalid_argument exception if no number is found
-int parseInt( const std::string& str, int start, int end );
+int parseInt( const std::string &str, int start, int end );
 
 // Removes every white space from a string
-void eraseWhiteSpaces( std::string& str );
+void eraseWhiteSpaces( std::string &str );
+
+// There are 3 type of statements special for us
+// we want to distinguish between them and the rest
+enum StatementType {
+    conditional,
+    unconditional,
+    stop,
+    other
+};
+
+// This defines the structure of the statements that will
+// be the node of our graph
+struct Node{
+    std::string statement;
+    StatementType type;
+    int labelToNextNode; // If the node is not of the type conditional
+                         // or unconditional this is -1
+};
+
+// These are "private" functions. They are used through the
+// "interpretStatement" function defined below
+// These functions are use to fill the type and labelToNextNode
+// of the node. In addition to that, the interpretIf and Goto remove
+// the label from the statement.
+// The node has to have the statement string with the
+// white spaces already removed.
+/*************************************************
+bool interpretIfStatement( Node &n );
+bool interpretGotoStatement( Node &n );
+bool interpretStopStatement( Node &n );
+void interpretOtherStatement( Node &n );
+*************************************************/
+
+// This uses the 4 interpret "private" functions above to determine
+// the type and labelToNextNode of the node. It also removes the
+// label in conditional and unconditional types from the statement.
+// As the functions above it needs the node to have the statement string
+// with no white spaces.
+void interpretStatement( Node &n );
 
 #endif
