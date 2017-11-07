03d1336cc0100d7f034442e90b2ec6a7ed76f19d
Carlos Domínguez García <alu0100966589@ull.edu.es>
2017-11-07 22:02:50 +0000

    Added some unit tests for parseLine function

--- test/test.cpp
+++ test/test.cpp
@@ -423,6 +423,179 @@ void testInterpretStatement()
 }
 
 
+
+void testParseLine()
+{
+    try{
+        {
+            std::string str = "      if(something)goto123";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "if(something)goto"
+            ||  l.n.type != StatementType::conditional
+            ||  l.n.labelToNextNode != 123
+            ||  l.lineLabel != -1
+            ||  l.lineHasLabel != false
+            ) throw "Test 1";
+        }
+
+        {
+            std::string str = "      if ( something ) goto 123";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "if(something)goto"
+            ||  l.n.type != StatementType::conditional
+            ||  l.n.labelToNextNode != 123
+            ||  l.lineLabel != -1
+            ||  l.lineHasLabel != false
+            ) throw "Test 2";
+        }
+
+        {
+            std::string str = "      if ( some t hing ) goto 123";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "if(something)goto"
+            ||  l.n.type != StatementType::conditional
+            ||  l.n.labelToNextNode != 123
+            ||  l.lineLabel != -1
+            ||  l.lineHasLabel != false
+            ) throw "Test 3";
+        }
+
+        {
+            std::string str = "12345 if(something)goto123";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "if(something)goto"
+            ||  l.n.type != StatementType::conditional
+            ||  l.n.labelToNextNode != 123
+            ||  l.lineLabel != 12345
+            ||  l.lineHasLabel != true
+            ) throw "Test 4";
+        }
+
+        {
+            std::string str = "99999 if( something )goto 123";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "if(something)goto"
+            ||  l.n.type != StatementType::conditional
+            ||  l.n.labelToNextNode != 123
+            ||  l.lineLabel != 99999
+            ||  l.lineHasLabel != true
+            ) throw "Test 5";
+        }
+
+        {
+            std::string str = "12345$if(something)goto123";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "if(something)goto"
+            ||  l.n.type != StatementType::conditional
+            ||  l.n.labelToNextNode != 123
+            ||  l.lineLabel != 12345
+            ||  l.lineHasLabel != true
+            ) throw "Test 6";
+        }
+
+        {
+            std::string str = "12345 stop";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "stop"
+            ||  l.n.type != StatementType::stop
+            ||  l.n.labelToNextNode != -1
+            ||  l.lineLabel != 12345
+            ||  l.lineHasLabel != true
+            ) throw "Test 7";
+        }
+
+        {
+            std::string str = "      stop";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "stop"
+            ||  l.n.type != StatementType::stop
+            ||  l.n.labelToNextNode != -1
+            ||  l.lineLabel != -1
+            ||  l.lineHasLabel != false
+            ) throw "Test 8";
+        }
+
+        {
+            std::string str = "12345 read 6, i, j, k";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "read6,i,j,k"
+            ||  l.n.type != StatementType::other
+            ||  l.n.labelToNextNode != -1
+            ||  l.lineLabel != 12345
+            ||  l.lineHasLabel != true
+            ) throw "Test 9";
+        }
+
+        {
+            std::string str = "   33 i = j";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "i=j"
+            ||  l.n.type != StatementType::other
+            ||  l.n.labelToNextNode != -1
+            ||  l.lineLabel != 33
+            ||  l.lineHasLabel != true
+            ) throw "Test 10";
+        }
+
+        {
+            std::string str = "   33 goto   1";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "goto"
+            ||  l.n.type != StatementType::unconditional
+            ||  l.n.labelToNextNode != 1
+            ||  l.lineLabel != 33
+            ||  l.lineHasLabel != true
+            ) throw "Test 11";
+        }
+
+        {
+            // The continuation marker doesn't have to be a white space
+            std::string str = "33   $goto 33";
+            Line l = parseLine( str );
+
+            if(
+                l.n.statement != "goto"
+            ||  l.n.type != StatementType::unconditional
+            ||  l.n.labelToNextNode != 33
+            ||  l.lineLabel != 33
+            ||  l.lineHasLabel != true
+            ) throw "Test 12";
+        }
+    }
+    catch( const char *msg )
+    {
+        std::cerr << "\ntestParseLine not working as expected. Failed " << msg << '\n';
+    }
+    catch( ... )
+    {
+        std::cerr << "\nUnknown error in testParseLine\n";
+    }
+
+}
+
+
 int main()
 {
     testEraseWhiteSpaces();
@@ -432,6 +605,7 @@ int main()
     testInterpretStopStatement();
     testInterpretOtherStatement();
     testInterpretStatement();
+    testParseLine();
 
     std::cout << "\n\nFinished unit tests\n\n";
     return 0;
